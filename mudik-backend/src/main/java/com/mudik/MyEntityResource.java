831889d669d946c0e060ecc29d68a713abc5dc03
khalil231004 <khalilgaming64@gmail.com>
2026-02-20 00:37:11 +0700

    perbaiki bug file

--- mudik-backend/src/main/java/com/mudik/MyEntityResource.java
+++
@@ -1,6 +0,0 @@
-package com.mudik;
-
-import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
-
-public interface MyEntityResource extends PanacheEntityResource<MyEntity, Long> {
-}
--- mudik-backend/src/test/java/com/mudik/model/PendaftaranMudikTest.java
+++
@@ -1,7 +0,0 @@
-package com.mudik.model;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class PendaftaranMudikTest {
-
-}
