1873eee951cc17acbee66b9c58db44fde4aea933
khalil231004 <khalilgaming64@gmail.com>
2026-02-20 01:10:23 +0700

    perbaiki cors 2

--- src/main/java/com/mudik/config/CorsFilter.java
+++ src/main/java/com/mudik/config/CorsFilter.java
@@ -11,7 +11,7 @@ public class CorsFilter implements ContainerResponseFilter {
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "https://seulamat.dishubaceh.com");
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
         responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, userid, x-requested-with");
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
         responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
